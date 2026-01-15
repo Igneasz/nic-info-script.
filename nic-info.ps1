34cb8156f526123601eddd287a90776b357c2b3b
Ignas Alionis <ignas.alioniscrystal@gmail.com>
2026-01-15 09:30:48 +0200

    Save network settings to nic-info-result.txt
    The script now saves network information to a file and provides user feedback.

--- nic-info.ps1
+++ nic-info.ps1
@@ -1,1 +1,6 @@
-Get-NetIPAddress | Format-Table
+# Išveda tinklo informaciją
+Write-Host "Tinklo nustatymai:" -ForegroundColor Green
+Get-NetIPAddress | Format-Table | Out-File nic-info-result.txt
+
+# Papildomai galite išvesti tekstą į failą
+Write-Host "Duomenys išsaugoti faile nic-info-result.txt." -ForegroundColor Yellow
